f8a90682ac5cc5e1f131718cbd0d024646989400
Marcio Carolino <marciocarolino@gmail.com>
2024-06-27 11:47:46 -0300

    feat: events start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ClientModule } from "./app/client/client.module";
 import { DataBaseModule } from "./db/entities/database.module";
+import { EventEmitterModule } from "@nestjs/event-emitter";
 
 @Module({
-  imports: [DataBaseModule, ClientModule],
+  imports: [EventEmitterModule.forRoot(), DataBaseModule, ClientModule],
   controllers: [],
   providers: [],
 })
